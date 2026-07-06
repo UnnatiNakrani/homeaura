d1fd18cdf1fce3ff10207672736f35c09060e5ee
UnnatiNakrani <unnatinakrani17@gmail.com>
2026-07-06 12:01:10 +0530

    authentication

--- src/helper/createAdmin.js
+++ src/helper/createAdmin.js
@@ -13,8 +13,6 @@ import { auth, db } from "../firebase";
 import { DEFAULT_ADMIN } from "../constant/CommonConstant";
 
 export const createAdmin = async () => {
-  console.log("function clllllll");
-
   try {
     // Check if admin already exists
     const q = query(
@@ -22,18 +20,12 @@ export const createAdmin = async () => {
       where("email", "==", DEFAULT_ADMIN.EMAIL)
     );
 
-
-    console.log(q, "qqq");
-
-
     const snapshot = await getDocs(q);
 
     snapshot.forEach((doc) => {
       console.log("Doc ID:", doc.id);
       console.log("Data:", doc.data());
     });
-    console.log(snapshot, snapshot.empty, "snapshots");
-
     if (!snapshot.empty) {
       console.log("Admin already exists");
       return;
@@ -46,7 +38,6 @@ export const createAdmin = async () => {
       DEFAULT_ADMIN.PASSWORD
     );
 
-    console.log(credential, "credentialcredentialcredentialcredential");
     await setDoc(doc(db, "users", credential.user.uid), {
       uid: credential.user.uid,
       // ...DEFAULT_ADMIN,
@@ -57,22 +48,8 @@ export const createAdmin = async () => {
       createdAt: new Date(),
     });
 
-
-    // Save Admin in Firestore
-    // await addDoc(collection(db, "users"), {
-    //   uid: credential.user.uid,
-    //   name: DEFAULT_ADMIN.NAME,
-    //   email: DEFAULT_ADMIN.EMAIL,
-    //   mobile: DEFAULT_ADMIN.MOBILE,
-    //   role: DEFAULT_ADMIN.ROLE,
-    //   createdAt: new Date(),
-    //   updatedAt: new Date(),
-    // });
-
     console.log("Admin Created");
   } catch (error) {
-
-    console.log(error, "errorerrorerrorerrorerror");
 
     console.log(error);
   }
